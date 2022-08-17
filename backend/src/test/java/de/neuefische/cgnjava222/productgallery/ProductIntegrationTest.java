b854010a702bcd3ee9be0cb668ce715498320466
dunjawohlers <dunjawohlers@gmx.de>
2022-08-17 12:34:30 +0200

    added product-IntegrationTest

---
+++ backend/src/test/java/de/neuefische/cgnjava222/productgallery/ProductIntegrationTest.java
@@ -0,0 +1,27 @@
+package de.neuefische.cgnjava222.productgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getProducts() throws Exception {
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/product-gallery/")
+                )
+                .andExpect(status().isOk());
+    }
+}
