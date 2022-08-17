5c1671a17c270d48e14499e92ebd2a4f267bdbb1
dunjawohlers <dunjawohlers@gmx.de>
2022-08-17 18:23:27 +0200

    review-fixes

--- backend/src/main/java/de/neuefische/cgnjava222/productgallery/service/ProductService.java
+++ backend/src/main/java/de/neuefische/cgnjava222/productgallery/service/ProductService.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 @Service
 public class ProductService {
+    String urlStart = "https://res.cloudinary.com/dcnqizhmg/image/upload";
     Product shopEntry1 = new Product("3a", "Roll-Brett",
             "Aus Holz, Massivholz",
-            List.of("https://res.cloudinary.com/dcnqizhmg/image/upload/v1660381069/hk39bkxodeg6kopmht9d.png"), 12, 1);
+            List.of(urlStart + "/v1660381069/hk39bkxodeg6kopmht9d.png"), 12, 1);
 
     public List<Product> getAllProducts() {
         return List.of(shopEntry1);
