a24488b2301392307120fd2d66420b19f932c283
dunjawohlers <dunjawohlers@gmx.de>
2022-08-17 18:14:04 +0200

    added headr and footer frontend, installed mui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 
 function App() {
-  return (
-    <div className="App">
-        Hallo DU!
-    </div>
+    return (
+        <div className="App">
+            <Header/>
+            <main>
+
+            </main>
+            <Footer/>
+        </div>
   );
 }
 
