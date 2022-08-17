50ba0867b4b39f53f2c07f23a0bff9972ad9e6ff
dunjawohlers <dunjawohlers@gmx.de>
2022-08-17 10:13:48 +0200

    Created Product type and class frontend, backend

---
+++ backend/src/main/java/de/neuefische/cgnjava222/productgallery/model/Product.java
@@ -0,0 +1,12 @@
+package de.neuefische.cgnjava222.productgallery.model;
+
+import java.util.List;
+
+public record Product(String id,
+                      String title,
+                      String description,
+                      List<String> urls,
+                      Integer price,
+                      Integer availableCount
+) {
+}
