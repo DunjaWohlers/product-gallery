a64e31332268c42f2673645389d1c2e16fc76ea5
dunjawohlers <dunjawohlers@gmx.de>
2022-08-18 12:59:01 +0200

    added Test for UUID Construction in Product-Record

---
+++ backend/src/test/java/de/neuefische/cgnjava222/productgallery/model/ProductTest.java
@@ -0,0 +1,20 @@
+package de.neuefische.cgnjava222.productgallery.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductTest {
+
+
+    @Test
+    void nonCanonicalArgsConstructorCreatesID() {
+        Product product = new Product("Biber", "knuffig, flauschig", List.of("http://google.de"), 4, 5);
+        String productID = product.id();
+        Integer expected = 36;
+        Integer actual = productID.length();
+        assertThat(actual).isEqualTo(expected);
+    }
+}
