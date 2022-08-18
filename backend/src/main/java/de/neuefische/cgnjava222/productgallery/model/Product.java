26cec44f8972f2c4b0960065b87fb8a6750f8daa
dunjawohlers <dunjawohlers@gmx.de>
2022-08-18 12:17:38 +0200

    new ProductServiceTest getProducts added non-canonical Args Constructor to Product with random UUID

--- backend/src/main/java/de/neuefische/cgnjava222/productgallery/model/Product.java
+++ backend/src/main/java/de/neuefische/cgnjava222/productgallery/model/Product.java
@@ -3,6 +3,7 @@ package de.neuefische.cgnjava222.productgallery.model;
 import org.springframework.data.annotation.Id;
 
 import java.util.List;
+import java.util.UUID;
 
 public record Product(
         @Id
@@ -13,4 +14,8 @@ public record Product(
         Integer price,
         Integer availableCount
 ) {
+        public Product(String title, String description, List<String> pictureUrls, Integer price, Integer availableCount) {
+                this(UUID.randomUUID().toString(), title, description, pictureUrls, price, availableCount);
+
+        }
 }
