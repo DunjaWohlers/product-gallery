165beb4d7f1867334947ab3f30648815c805429a
dunjawohlers <dunjawohlers@gmx.de>
2022-08-21 19:07:24 +0200

    graphical changes in header, added not existing free line at end of documents

--- frontend/src/component/HeaderNav.tsx
+++ frontend/src/component/HeaderNav.tsx
@@ -1,10 +1,11 @@
 import {NavLink} from "react-router-dom";
+import icon from "../sawIcon.png";
 
 export default function HeaderNav() {
 
     return <header>
-        <div id="logo"></div>
-        <h1><NavLink className={"navLink"} to={"/"}> Holz - Werkstatt </NavLink></h1>
+        <div id="logo"><img src={icon} alt={"icon"}/></div>
+        <NavLink className={"navLink"} to={"/"}><h1> Product - Gallery </h1></NavLink>
         <button>Search</button>
         <button>Login</button>
     </header>
