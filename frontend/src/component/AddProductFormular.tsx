221757285be9b26abdc7e256ab78995bcba2d44a
dunjawohlers <dunjawohlers@gmx.de>
2022-08-24 11:21:15 +0200

    feature: added warning if picture-url is not valid when saving

--- frontend/src/component/AddProductFormular.tsx
+++ frontend/src/component/AddProductFormular.tsx
@@ -2,6 +2,7 @@ import {NewProduct, Product} from "../type/Product";
 import {FormEvent, useState} from "react";
 import "./editAddDetails.css";
 import {toast} from "react-toastify";
+import axios from "axios";
 
 type AddProductFormProps = {
     addProduct: (newProduct: NewProduct) => Promise<Product | void>
@@ -16,15 +17,25 @@ export default function AddProductFormular(props: AddProductFormProps) {
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (pictureUrls) {
+            checkImage(pictureUrls[0]);
+        }
         if (title && description && pictureUrls && price && availableCount &&
             title.length > 0 && description.length > 0 && pictureUrls.length > 0) {
+            checkImage(pictureUrls[0]);
             props.addProduct({title, description, pictureUrls, price, availableCount})
-                .then(() =>
-                    toast.success("Produkt wurde gespeichert!", {theme: "light"}))
+                .then(() => toast.success("Produkt wurde gespeichert!", {theme: "light"}))
                 .catch(() => toast.error("Produkt konnte nicht gespeichert werden!", {theme: "light"}));
         } else {
             toast.info("Produktinformationen fehlen!", {theme: "light"});
         }
+    }
+
+    const checkImage = (url: string) => {
+        axios.get(url)
+            .then(result => result.headers)
+            .then((() => toast.success("Bildpfad ist erreichbar.")))
+            .catch(() => toast.warn("Bild-URL ist nicht erreichbar ... überprüfe die Eingabe!"))
     }
 
     return <>
