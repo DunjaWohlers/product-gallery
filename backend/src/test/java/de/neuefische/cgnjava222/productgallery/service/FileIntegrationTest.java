8573f622cfaf68a14585fc47c2ba595dc35a770c
dunjawohlers <dunjawohlers@gmx.de>
2022-08-27 12:10:30 +0200

    test fileintegration upload fixed

--- backend/src/test/java/de/neuefische/cgnjava222/productgallery/service/FileIntegrationTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/productgallery/service/FileIntegrationTest.java
@@ -1,9 +1,9 @@
 package de.neuefische.cgnjava222.productgallery.service;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -13,23 +13,30 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.File;
+import java.util.Map;
+
 import static com.mongodb.assertions.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest
-@AutoConfigureMockMvc
-public class FileIntegrationTest {
+class FileIntegrationTest {
 
-    //@Autowired
-    //private MockMvc mockMvc;
+    // @Autowired
+    // @InjectMocks
+    // private MockMvc mockMvc;
 
     @MockBean
     private Cloudinary cloudinary;
 
-    //@MockBean
-    //private Uploader uploader;
+    @MockBean
+    private Uploader uploader;
+
     @Autowired
     private WebApplicationContext webApplicationContext;
 
@@ -41,11 +48,19 @@ public class FileIntegrationTest {
                 MediaType.TEXT_PLAIN_VALUE,
                 "Hello, World!".getBytes());
         assertNotNull(firstFile);
+        when(cloudinary.uploader()).thenReturn(uploader);
+        when(uploader
+                .upload(
+                        any(File.class),
+                        anyMap()
+                )
+        )
+                .thenReturn(Map.of("url", "hi", "public_id", "bla"));
 
         MockMvc mockMvc
                 = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         mockMvc.perform(multipart("/api/image/uploadFile/").file(firstFile))
-                .andExpect(status().isOk());
+                .andExpect(status().isCreated());
     }
 }
 
