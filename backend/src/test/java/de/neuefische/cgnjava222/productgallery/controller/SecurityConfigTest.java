9bc6e01f5dfb7d581133a37c3bcac0faee5d10fb
dunjawohlers <dunjawohlers@gmx.de>
2022-09-06 16:36:16 +0200

    try to fix problem with anonymous user who can upload pictures on cloudinary?

--- backend/src/test/java/de/neuefische/cgnjava222/productgallery/controller/SecurityConfigTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/productgallery/controller/SecurityConfigTest.java
@@ -1,28 +1,37 @@
 package de.neuefische.cgnjava222.productgallery.controller;
 
+import com.cloudinary.Api;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.File;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 class SecurityConfigTest {
-
-    @Autowired
-    private WebApplicationContext context;
 
     @Autowired
     private MockMvc mockMvc;
@@ -61,20 +70,40 @@ class SecurityConfigTest {
                 .andExpect(status().isUnauthorized());
     }
 
-    // @Test
-    // @WithAnonymousUser
-    // void tryToAddImageAsAnonymousUser() throws Exception {
-    //     MockMultipartFile firstFile = new MockMultipartFile(
-    //             "file", "sawIcon.png",
-    //             MediaType.TEXT_PLAIN_VALUE,
-    //             "Hello, World!".getBytes());
-    //     assertNotNull(firstFile);
 
-    //     FileService fileService = mock(FileService.class);
-    //     MultipartFile[] multipartArray = new MultipartFile[]{firstFile};
+    @MockBean
+    private Cloudinary cloudinary;
 
-    //     when(fileService.uploadPictures(multipartArray)).thenReturn(List.of(new ImageInfo("bla", "blubbb")));
+    @MockBean
+    private Uploader uploader;
 
-    //     mockMvc.perform(post("/api/image/uploadFile").
-    // }
+    @MockBean
+    private Api api;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    @WithAnonymousUser
+    void tryToAddImageAsAnonymousUser() throws Exception {
+        MockMultipartFile firstFile = new MockMultipartFile(
+                "file", "sawIcon.png",
+                MediaType.TEXT_PLAIN_VALUE,
+                "Hello, World!".getBytes());
+        assertNotNull(firstFile);
+
+        when(cloudinary.uploader()).thenReturn(uploader);
+        when(uploader
+                .upload(
+                        any(File.class),
+                        anyMap()
+                )
+        ).thenReturn(Map.of("url", "hi", "publicId", "bla"));
+
+        MockMvc mockMvc
+                = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+        mockMvc.perform(multipart("/api/image/uploadFile/").file(firstFile).with(csrf()))
+                .andExpect(status().isCreated());
+    }
 }
