06750c6a52a233cff4ed12d7c5c63e18d60f0b82
dunjawohlers <dunjawohlers@gmx.de>
2022-09-09 15:33:48 +0200

    add UserController

---
+++ backend/src/main/java/de/neuefische/cgnjava222/productgallery/controller/OrderController.java
@@ -0,0 +1,24 @@
+package de.neuefische.cgnjava222.productgallery.controller;
+
+import de.neuefische.cgnjava222.productgallery.model.SingleOrder;
+import de.neuefische.cgnjava222.productgallery.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping("")
+    public List<SingleOrder> getOrdersFromUser(@PathVariable String userName) {
+        return orderService.getMyOrder(userName);
+    }
+}
