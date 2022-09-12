bca5f5bb277c1f2c552d58a0ccf01fbbb446d134
dunjawohlers <dunjawohlers@gmx.de>
2022-09-12 09:31:32 +0200

    style fix (back to old, working structure)

--- frontend/src/component/DetailsProduct.tsx
+++ frontend/src/component/DetailsProduct.tsx
@@ -33,52 +33,57 @@ export default function DetailsProduct(props: DetailsProductProps) {
     }
 
     return (<>
-            {thisProduct && <div className={"fullView details"}>
-                <h3>{thisProduct.title}</h3>
-                <img src={thisProduct.pictureObj[0].url} alt={"Loading..."}/>
-                {thisProduct.pictureObj &&
-                    <div className={"flexBox"}>
-                        {thisProduct.pictureObj.length > 1 &&
-                            <div className={"cardContainer"}>
-                                <div className={"imageContainer"}>
-                                    <img alt="bild1" src={thisProduct.pictureObj[1].url}/>
-                                </div>
-                            </div>
-                        }
-                        {thisProduct.pictureObj.length > 2 &&
-                            <div className={"cardContainer"}>
-
-                                <div className={"imageContainer"}>
-                                    <img alt="bild2" src={thisProduct.pictureObj[2].url}/>
-                                </div>
-                            </div>
-                        }
-                        {thisProduct.pictureObj.length > 3 &&
-                            <div className={"cardContainer"}>
-                                <div className={"imageContainer"}>
-                                    <img alt="bild3" src={thisProduct.pictureObj[3].url}/>
-                                </div>
-                            </div>
-                        }
+            {thisProduct &&
+                <div className={"details"}>
+                    <h3>{thisProduct.title}</h3>
+                    <button id="saveFavouriteButton" onClick={handleSave}> *</button>
+                    <div className={"cardContainer"}>
+                        <div className={"imageContainer"}>
+                            <img src={thisProduct.pictureObj[0].url} alt={"Loading..."}/>
+                        </div>
                     </div>
-                }
-                <button onClick={handleSave}> merken</button>
-                <p>{thisProduct.description}</p>
-                <p>{thisProduct?.price} &euro;</p>
-                {thisProduct.availableCount > 0 &&
-                    <><p> Dieses Produkt ist vorrätig.</p>
-                        <p> Anzahl vorrätiger Produkte: {thisProduct.availableCount} </p></>
-                }
-                {thisProduct.availableCount === 0 && <p> Dieses Produkt kann hergestellt werden,
-                    genauere Details zur Dauer hängen von vielen Faktoren ab und müssen
-                    erfragt werden. </p>
-                }
-                {thisProduct.availableCount < 0 &&
-                    <p> Dieses Produkt wurde aus dem Angebot entfernt. Schreiben Sie gerne eine
-                        Nachricht, wenn Sie es dennoch wünschen, bei genügend Anfragen kann es
-                        eventuell wieder angeboten werden. </p>
-                }
-            </div>}
+                    {thisProduct.pictureObj &&
+                        <div className={"images3flex"}>
+
+                            {thisProduct.pictureObj.length > 1 &&
+                                <div className={"cardContainer"}>
+                                    <div className={"imageContainer"}>
+                                        <img alt="bild1" src={thisProduct.pictureObj[1].url}/>
+                                    </div>
+                                </div>
+                            }
+                            {thisProduct.pictureObj.length > 2 &&
+                                <div className={"cardContainer"}>
+                                    <div className={"imageContainer"}>
+                                        <img alt="bild2" src={thisProduct.pictureObj[2].url}/>
+                                    </div>
+                                </div>
+                            }
+                            {thisProduct.pictureObj.length > 3 &&
+                                <div className={"cardContainer"}>
+                                    <div className={"imageContainer"}>
+                                        <img alt="bild3" src={thisProduct.pictureObj[3].url}/>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                    }
+                    <p>{thisProduct.description}</p>
+                    <p>{thisProduct?.price} &euro;</p>
+                    {thisProduct.availableCount > 0 &&
+                        <><p> Dieses Produkt ist vorrätig.</p>
+                            <p> Anzahl vorrätiger Produkte: {thisProduct.availableCount} </p></>
+                    }
+                    {thisProduct.availableCount === 0 && <p> Dieses Produkt kann hergestellt werden,
+                        genauere Details zur Dauer hängen von vielen Faktoren ab und müssen
+                        erfragt werden. </p>
+                    }
+                    {thisProduct.availableCount < 0 &&
+                        <p> Dieses Produkt wurde aus dem Angebot entfernt. Schreiben Sie gerne eine
+                            Nachricht, wenn Sie es dennoch wünschen, bei genügend Anfragen kann es
+                            eventuell wieder angeboten werden. </p>
+                    }
+                </div>}
         </>
     )
 }
