ba786560e1fbd1d131092b9c85e0813e607f2924
dunjawohlers <dunjawohlers@gmx.de>
2022-09-12 16:15:47 +0200

    fix Test with DirtiesContext

--- backend/src/test/java/de/neuefische/cgnjava222/productgallery/OrderIntegrationTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/productgallery/OrderIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 @SpringBootTest
 class OrderIntegrationTest {
