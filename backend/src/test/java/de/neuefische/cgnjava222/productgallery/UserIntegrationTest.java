e4ace13e06b74cf414334cdaa9305a460cbf7214
dunjawohlers <dunjawohlers@gmx.de>
2022-09-13 16:38:44 +0200

    fix test

--- backend/src/test/java/de/neuefische/cgnjava222/productgallery/UserIntegrationTest.java
+++ backend/src/test/java/de/neuefische/cgnjava222/productgallery/UserIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserIntegrationTest {
 
     @Autowired
