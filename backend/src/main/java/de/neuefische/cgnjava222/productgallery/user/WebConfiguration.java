5535d119c3a9604f6924e293edde089ff7ca1a62
Steppzilla <45403455+Steppzilla@users.noreply.github.com>
2022-11-11 17:30:28 +0100

    web config

--- backend/src/main/java/de/neuefische/cgnjava222/productgallery/user/WebConfiguration.java
+++ backend/src/main/java/de/neuefische/cgnjava222/productgallery/user/WebConfiguration.java
@@ -11,5 +11,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/{spring:\\w+}")
                 .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/{spring:\\w+}")
+                .setViewName("forward:/");
     }
 }
